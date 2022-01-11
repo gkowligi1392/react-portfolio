0c78243de076454714c35509f88ba1c70c92d0a9
Gokul's Laptop <gkowligi@gmail.com>
2022-01-11 13:02:30 -0800

    added navigation functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import About from "./components/About";
 import ContactMe from "./components/ContactMe";
@@ -5,15 +6,17 @@ import Nav from "./components/Nav";
 import Portfolio from "./components/Portfolio";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("aboutMe");
+
   return (
     <>
       <header>
-        <Nav />
+        <Nav currentPage={currentPage} setCurrentPage={setCurrentPage}/>
       </header>
       <main>
-        <ContactMe />
-        <About />
-        <Portfolio />
+        {currentPage === "aboutMe" && <About />}
+        {currentPage === "portfolio" && <Portfolio />}
+        {currentPage === "contactMe" && <ContactMe />}
       </main>
       <footer>this is the footer</footer>
     </>
