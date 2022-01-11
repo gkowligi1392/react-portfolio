9c1a7d16c15f82b55bc38f8c214e91f0ad9a8845
Gokul's Laptop <gkowligi@gmail.com>
2022-01-11 00:17:35 -0800

    added initial styling and navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,36 @@
+import AboutMeImage from "../../assets/images/about-me.svg";
+import ContactMeImage from "../../assets/images/contact-me.svg";
+import Logo from "../../assets/images/logo.svg";
+import PortafolioImage from "../../assets/images/portafolio.svg";
+import ResumeImage from "../../assets/images/resume.svg";
+
 const Nav = () => {
-    return (
+  return (
+    <>
+      <div className="header-logo">
+        <img src={Logo} className="img-logo" alt="logo" />
+        <h1>Joseduardo</h1>
+      </div>
       <nav>
-        <div className="nav-logo">Logo</div>
-        <div className="nav-item">About Me</div>
-        <div className="nav-item">Portafolio</div>
-        <div className="nav-item">Contact Me</div>
-        <div className="nav-item">Resume</div>
+        <div className="nav-item">
+          <img src={AboutMeImage} className="img-nav" alt="about-me" />
+          About Me
+        </div>
+        <div className="nav-item">
+          <img src={PortafolioImage} className="img-nav" alt="portafolio" />
+          Portafolio
+        </div>
+        <div className="nav-item">
+          <img src={ContactMeImage} className="img-nav" alt="contact-me" />
+          Contact Me
+        </div>
+        <div className="nav-item">
+          <img src={ResumeImage} className="img-nav" alt="resume" />
+          Resume
+        </div>
       </nav>
-    );
-  };
-  
-  export default Nav;
+    </>
+  );
+};
+
+export default Nav;
